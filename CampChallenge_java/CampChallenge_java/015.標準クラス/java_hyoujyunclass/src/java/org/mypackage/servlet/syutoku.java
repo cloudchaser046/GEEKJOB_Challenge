954d3b2516ecbceab2b9fb8ebc2fdb2af1ca79ec
YU\jung_yucheol <cloud.chaser.046.vxcf.2@gmail.com>
2017-10-17 00:31:48 +0900

    JAVA 課題 文字数の取得を提出します

---
+++ CampChallenge_java/CampChallenge_java/015.標準クラス/java_hyoujyunclass/src/java/org/mypackage/servlet/syutoku.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.servlet;
+
+/**
+ *
+ * @author jung_yucheol
+ */
+public class syutoku {
+    public static void main(String[] args){
+    String mail = "cloud.chaser@gmail.com";
+    System.out.print(mail.substring(13));
+    }
+}
